3f5293d11352258ef7c6c562776b836929214774
Dhruvkumar Shah <dhruvkumar.shah.23@gmail.com>
2019-12-30 19:02:17 -0500

    intial commit for register

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -9,25 +11,91 @@ import { auth } from 'firebase/app';
 })
 export class RegisterPage implements OnInit {
 
-  username: string = "";
-  password: string = "";
-  cpassword: string = "";
-  constructor( public afAuth: AngularFireAuth) { }
+  email: string;
+  password: string;
+  cpassword: string;
+  registerForm: FormGroup;
+  avatarSrc = '../../assets/icon/default_profile.svg';
+  isProfilePicSelected = false;
+  
+  @ViewChild('profilePic', {static: true}) profilePic: ElementRef;
+
+  constructor(private afAuth: AngularFireAuth, private fb: FormBuilder, private navCtrl: NavController) { }
 
   ngOnInit() {
+    this.registerForm = this.fb.group({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+      confirmPassword: new FormControl('', Validators.required)
+    }, {
+      validator: this.mustMatch('password', 'confirmPassword')
+    });
   }
 
-  async register() {
-    const {username, password, cpassword} = this;
-    console.log(this);
-    if (password !== cpassword){
-      return console.error("Password doesn't match!");
-    }
-      try{
-        const res = await this.afAuth.auth.createUserWithEmailAndPassword(username,password);
-        console.log(res);
-      }catch(error){
-        console.dir(error);
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        return;
       }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    }
   }
+
+  get controls() { return this.registerForm.controls; }
+
+  async register() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+    // try {
+    //   const res = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+    //   console.log(res);
+    // } catch (error) {
+    //   console.dir(error);
+    // }
+  }
+
+  previewProfilePic(file: File) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = (event) => {
+      this.profilePic.nativeElement.src = event.target.result as string;
+      this.isProfilePicSelected = true;
+    }
+  }
+
+  picChange(event: any) {
+    let file: File = event.srcElement.files[0];
+    this.previewProfilePic(file);
+  
+    // var storageRef = firebase.storage().ref(`images/${this.afAuth.auth.currentUser.uid}.jpg`);
+    // console.log(this.afAuth.auth.currentUser.uid);
+    // var uploadTask = storageRef.put(file).then((snapshot) => {
+    //   snapshot.ref.getDownloadURL().then((url) => {
+    //     this.auth.firedata.child(`${this.afAuth.auth.currentUser.uid}`).update({
+    //       'photoURL': url
+    //     }).then(() => {
+    //       this.setRoot();
+    //     });
+    //   });
+    // });
+  }
+
+  loginPage() {
+    this.navCtrl.pop();
+  }
+
 }
