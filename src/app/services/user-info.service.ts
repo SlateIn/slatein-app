70deb98c3c72935f1feca7c041e4610102e886af
Dhruvkumar Shah <dhruvkumar.shah.23@gmail.com>
2020-01-25 10:03:06 -0500

    Restructured data entry for user profile

--- src/app/services/user-info.service.ts
+++ src/app/services/user-info.service.ts
@@ -15,7 +15,7 @@ export class UserService {
    get info(): Observable<UserInfo> {
     return this.afAuth.authState.pipe(
       map(auth => auth.uid),
-      switchMap(res => this.db.object<UserInfo>(`/users/${res}`).valueChanges()));
+      switchMap(res => this.db.object<UserInfo>(`/users/${res}/profile`).valueChanges()));
    }
 
 }
