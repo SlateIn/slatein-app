75b80488c0415bbf18d0f80c7cd4e552b94b5fb8
Dhruvkumar Shah <dhruvkumar.shah.23@gmail.com>
2020-01-25 10:00:27 -0500

    Added Scheduler Count for unique id

--- src/app/services/local-notifications.service.ts
+++ src/app/services/local-notifications.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
-
+import * as firebase from 'firebase';
 import { Plugins } from '@capacitor/core';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { switchMap, map } from 'rxjs/operators';
+import { AngularFireDatabase } from '@angular/fire/database';
 const { LocalNotifications } = Plugins;
 
 
@@ -9,7 +13,22 @@ const { LocalNotifications } = Plugins;
 })
 export class LocalNotificationsService {
 
-  constructor() { }
+  firedata = firebase.database().ref('/users/');
+
+  
+  constructor(private afAuth: AngularFireAuth, private db: AngularFireDatabase) { }
+
+  getScheduleCounter(): Observable<number> {
+    return this.afAuth.authState.pipe(
+      map(auth => auth.uid),
+      switchMap(res => this.db.object<number>(`/users/${res}/scheduleCounter`).valueChanges()));
+   }
+
+  setScheduleCounter(counter: number) {
+    return this.firedata.child(`${this.afAuth.auth.currentUser.uid}`).update({
+      scheduleCounter: counter
+    });
+   }
 
   scheduleAt(title: string, body: string, id: number, scheduelAt: Date) {
     return LocalNotifications.schedule({
