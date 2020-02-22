7bf6837d031f886703205d7642f6652607ee218c
tanmay30 <pateltanmay30@gmail.com>
2020-02-22 12:50:45 -0500

    Initial commit-dhruv

--- src/app/calendar/calendar.page.ts
+++ src/app/calendar/calendar.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
- 
+
 import { Ionic4DatepickerModalComponent } from '@logisticinfotech/ionic4-datepicker';
 
 @Component({
@@ -18,12 +18,12 @@ export class CalendarPage implements OnInit {
   calendar = {
     mode: 'month',
     currentDate: new Date()
-  }; 
+  };
 
   constructor(
     public modalCtrl: ModalController
   ) { }
-  
+
 
   ngOnInit() {
     // this.datePickerObj = {
@@ -39,7 +39,7 @@ export class CalendarPage implements OnInit {
     this.viewTitle = title;
   }
 
-  onCurrentDateChanged(event:Date) {
+  onCurrentDateChanged(event: Date) {
     var today = new Date();
     today.setHours(0, 0, 0, 0);
     event.setHours(0, 0, 0, 0);
@@ -52,12 +52,13 @@ export class CalendarPage implements OnInit {
 
   onTimeSelected(ev) {
     console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
-        (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+      (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+    //this.changeMode('day');
   }
 
   changeMode(mode) {
     this.calendar.mode = mode;
-  } 
+  }
 
   // async openDatePicker() {
   //   const datePickerModal = await this.modalCtrl.create({
@@ -69,7 +70,7 @@ export class CalendarPage implements OnInit {
   //     }
   //   });
   //   await datePickerModal.present();
- 
+
   //   datePickerModal.onDidDismiss()
   //     .then((data) => {
   //       console.log(data);
@@ -80,43 +81,43 @@ export class CalendarPage implements OnInit {
   createRandomEvents() {
     var events = [];
     for (var i = 0; i < 50; i += 1) {
-        var date = new Date();
-        var eventType = Math.floor(Math.random() * 2);
-        var startDay = Math.floor(Math.random() * 90) - 45;
-        var endDay = Math.floor(Math.random() * 2) + startDay;
-        var startTime;
-        var endTime;
-        if (eventType === 0) {
-            startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
-            if (endDay === startDay) {
-                endDay += 1;
-            }
-            endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
-            events.push({
-                title: 'All Day - ' + i,
-                startTime: startTime,
-                endTime: endTime,
-                allDay: true
-            });
-        } else {
-            var startMinute = Math.floor(Math.random() * 24 * 60);
-            var endMinute = Math.floor(Math.random() * 180) + startMinute;
-            startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
-            endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
-            events.push({
-                title: 'Event - ' + i,
-                startTime: startTime,
-                endTime: endTime,
-                allDay: false
-            });
+      var date = new Date();
+      var eventType = Math.floor(Math.random() * 2);
+      var startDay = Math.floor(Math.random() * 90) - 45;
+      var endDay = Math.floor(Math.random() * 2) + startDay;
+      var startTime;
+      var endTime;
+      if (eventType === 0) {
+        startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
+        if (endDay === startDay) {
+          endDay += 1;
         }
+        endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
+        events.push({
+          title: 'All Day - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: true
+        });
+      } else {
+        var startMinute = Math.floor(Math.random() * 24 * 60);
+        var endMinute = Math.floor(Math.random() * 180) + startMinute;
+        startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
+        endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
+        events.push({
+          title: 'Event - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: false
+        });
       }
-      return events;
+    }
+    return events;
   }
   onRangeChanged(ev) {
-      console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
+    console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
   }
-  markDisabled = (date:Date) => {
+  markDisabled = (date: Date) => {
     var current = new Date();
     current.setHours(0, 0, 0);
     return date < current;
