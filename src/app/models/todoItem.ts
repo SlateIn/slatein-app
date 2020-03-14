74bf3cb29d0f428e8869bd3d023eb6b670364e6c
Sharmil <sharmilpatel7171@gmail.com>
2020-03-14 11:58:25 -0400

    ToDo Services

---
+++ src/app/models/todoItem.ts
@@ -0,0 +1,5 @@
+export interface ToDoItem {
+    id?: number;
+    listItem: string;
+    completed: boolean;
+}
--- src/app/planner/planner.page.ts
+++ src/app/planner/planner.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoList } from '@models/todoList';
+import { ToDoService } from '@services/todo.service';
+import { ToDoItem } from '@models/todoItem';
 
 @Component({
   selector: 'app-planner',
@@ -6,10 +9,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planner.page.scss'],
 })
 export class PlannerPage implements OnInit {
+  item1: ToDoItem;
+  item2: ToDoItem;
+  item3: ToDoItem;
 
-  constructor() { }
+  public toDoList: ToDoList = {
+    id: undefined,
+    listName: undefined,
+    listItems: undefined
+  };
+
+  constructor(private toDoService: ToDoService) { }
 
   ngOnInit() {
+    this.item1 = {
+      id: 1,
+      listItem: 'First List',
+      completed: false
+    };
+
+    this.item2 = {
+      id: 1,
+      listItem: 'Second List',
+      completed: false
+    };
+
+    this.item3 = {
+      id: 1,
+      listItem: 'Third List',
+      completed: false
+    };
+
+    this.toDoList.id = 123;
+    this.toDoList.listName = "My First List";
+    this.toDoList.listItems = [this.item1, this.item2];
+
+    console.log(this.toDoList);
   }
 
+  onAddList() {
+    console.log(this.toDoList);
+    this.toDoService.createToDoList(this.toDoList).then(list => {
+      console.log(list);
+    });
+
+  }
+
+  onUpdateList() {
+    this.toDoList.listItems.push(this.item3)
+
+    console.log(this.toDoList);
+    this.toDoService.updateToDoList(this.toDoList).then(list => {
+      console.log(list);
+    });
+  }
+
+  onDeleteList() {
+    console.log(this.toDoList);
+    this.toDoService.deleteToDoList(this.toDoList);
+  }
 }
