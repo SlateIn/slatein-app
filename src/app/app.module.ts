cd234cfe18e78c3efe5118d6576d16eedf3f5357
Raxit Patel <raxitp02@gmail.com>
2020-04-26 19:50:01 -0400

    ST-78 prettier eslint configuration for slatein

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DatePipe } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DatePipe} from '@angular/common';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AngularFireModule} from '@angular/fire';
-import { AngularFireDatabaseModule} from '@angular/fire/database';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment} from '../environments/environment';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {environment} from '../environments/environment';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgCalendarModule  } from 'ionic2-calendar';
-import { SettingsPage } from './profile/settings/settings.page';
-import { PersonalInformationPage } from './profile/personal-information/personal-information.page';
-import { SecurityPasswordPage } from './profile/security-password/security-password.page';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgCalendarModule} from 'ionic2-calendar';
+import {SettingsPage} from './profile/settings/settings.page';
+import {PersonalInformationPage} from './profile/personal-information/personal-information.page';
+import {SecurityPasswordPage} from './profile/security-password/security-password.page';
 
 // ionic4-datepicker
-import { Ionic4DatepickerModule } from '@logisticinfotech/ionic4-datepicker';
-
-
+import {Ionic4DatepickerModule} from '@logisticinfotech/ionic4-datepicker';
 
 @NgModule({
   declarations: [AppComponent, SettingsPage, PersonalInformationPage, SecurityPasswordPage],
@@ -35,7 +33,7 @@ import { Ionic4DatepickerModule } from '@logisticinfotech/ionic4-datepicker';
     NgCalendarModule,
     FormsModule,
     IonicModule.forRoot({
-      mode: 'ios'
+      mode: 'ios',
     }),
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
@@ -44,12 +42,7 @@ import { Ionic4DatepickerModule } from '@logisticinfotech/ionic4-datepicker';
     ReactiveFormsModule,
     Ionic4DatepickerModule,
   ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    DatePipe
-  ],
-  bootstrap: [AppComponent]
+  providers: [StatusBar, SplashScreen, {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}, DatePipe],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
