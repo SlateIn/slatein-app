167dfef83ebbe11590cae2823357ceaf3854121f
Jay Desai <jaydesai1417@gmail.com>
2020-04-28 10:52:49 -0400

    ADD Fonts, Primary, Danger and Success colors. Add "Add" button on my day page

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertReminderService } from '@app/myday/services/alert-reminder.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-  constructor() {}
+  constructor(private alertReminderService: AlertReminderService) {}
+
+  setReminder() {
+    this.alertReminderService.presentAlertPrompt('Add');
+  }
 }
