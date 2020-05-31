a07d899e93c7550e1d066bf3479d9ebac6623a25
Your Name <you@example.com>
2020-05-31 07:18:28 -0700

    fixing bug

--- src/app/login/register/register.page.ts
+++ src/app/login/register/register.page.ts
@@ -7,6 +7,7 @@ import { PasswordValidatorService } from '@services/password-validator.service';
 import { Subscription } from 'rxjs';
 import { CameraService } from '@services/camera.service';
 import { DatePipe } from '@angular/common';
+import { SafeResourceUrl } from '@angular/platform-browser';
 
 const { Storage } = Plugins;
 @Component({
@@ -24,6 +25,7 @@ export class RegisterPage implements OnInit {
   singUpFailedErrorMsg: string;
   formValueChangesSubscription: Subscription;
   photoBase64: string;
+  photo: SafeResourceUrl;
   currentDate = new Date().toLocaleDateString();
 
   @ViewChild('profilePic', { static: false }) profilePicRef: ElementRef;
