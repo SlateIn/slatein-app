db71a24cf6544cca1de4ab74e511d29a3ba903eb
Jay Desai <jaydesai1417@gmail.com>
2020-07-26 18:10:19 -0400

    work in progress

--- src/app/login/create-profile/create-profile.module.ts
+++ src/app/login/create-profile/create-profile.module.ts
@@ -4,11 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ComponentsModule } from '@app/components/header';
 import { CreateProfilePageRoutingModule } from './create-profile-routing.module';
+import { CreateProfileComponent } from './create-profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CreateProfileComponent],
   imports: [
     CommonModule, FormsModule, IonicModule, ReactiveFormsModule, ComponentsModule, CreateProfilePageRoutingModule
   ]
